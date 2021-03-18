7c8febf77dc81cb94063f89c85a8d743def86445
ChristianGroeber <christian.groeber@bluewin.ch>
2021-03-18 16:58:07 +0100

    initial commit

---
+++ create-skeleton.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Project Name"
+read projectName
+
+cp -r ./skeletonacho/root $projectName
+
+cd $projectName
+
+echo "Port"
+read port
+
+printf "version: \"3\"\n\nservices:\n   nacho:\n        build:\n            context: .\n            dockerfile: Dockerfile\n        container_name: $projectName\n        ports:\n        - $port:80\n        volumes:\n        - ./:/var/www/html" > docker-compose.yaml
+
+docker-compose up -d
+docker exec -it ${projectName} bash <<<EOF
+composer install
+exit
+EOF
